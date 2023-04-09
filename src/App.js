a81a58a0c174659741b2f07886a2eb69f8158f15
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-04-09 11:17:21 -0400

    Add and delete tasks works also prud build set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-function App() {
-  const [task, setTasks] = useState([
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       text: " Doctor Appointment",
@@ -23,21 +25,40 @@ function App() {
       reminder: false,
     },
   ]);
-
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    console.log(id);
+    setTasks([...tasks, newTask]);
+  };
   //Delete Task
   const deleteTask = (id) => {
-    setTasks(task.filter((task) => task.id !== id));
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
   return (
     <div className="container">
-      <Header />
-      {task.length > 0 ? (
-        <Tasks tasks={task} onDelete={deleteTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
     </div>
   );
-}
+};
 
 export default App;
